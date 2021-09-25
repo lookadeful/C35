62c4e1b62ad7e04bba28bb6c11ef8b4b7701ee75
lookadeful <31774919+lookadeful@users.noreply.github.com>
2021-09-25 17:14:56 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+var balloon,balloonImage1,balloonImage2;
+var height = 0
+// crea aquí la base de datos y la variable de posición 
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Función para configurar el entorno inicial
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,550,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonHeight = database.ref('balloon/height');
+  balloonHeight.on("value", updateHeight, showError);
+  textSize(20); 
+}
+
+// función para mostrar la Interfaz del Usuario (UI por sus siglas en inglés)
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //escribe el código para mover el globo aerostático en dirección hacia la izquierda
+    balloon.velocityX = -5;
+    updateHeight(-10, 0)
+  }
+  if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //escribe el código para mover el globo aerostático en dirección hacia la derecha
+    balloon.velocityX = 5;
+    updateHeight(10, 0)
+  }
+  if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //escribe el código para mover el globo aerostático en dirección ascendente
+    balloon.velocityY = -5
+    balloon.scale = balloon.scale + 0.001
+    updateHeight(0, -10)
+  }
+  if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //escribe el código para mover el globo aerostático en dirección descendente
+    balloon.velocityY = 5
+    balloon.scale = balloon.scale - 0.001
+    updateHeight(0, 10)
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**¡Utiliza las teclas de flecha para mover el globo aerostático!",40,40);
+}
+
+function updateHeight(x, y){
+  database.ref('balloon/height').set({
+    x: balloon.x,
+    y: balloon.y
+  })
+}
+
+function readHieght(data){
+  height = data.val();
+  console.log(height.x);
+  balloon.x = height.x;
+  balloon.y = height.y;
+}
+
+function showError(){
+  console.log("lol message me on github")
+}
